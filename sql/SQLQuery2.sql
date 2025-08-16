fb23a137a1b6d5b92edbc13151aece1074581a9f
hasti <104490662+hastitajik@users.noreply.github.com>
2025-08-16 19:47:17 +0330

    sql

---
+++ sql/SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿select * from weather_daily
+select * from power_usage;
+
+-- تعداد رکورد
+SELECT COUNT(*) FROM stg_energy_raw;
+
+-- بازه زمانی
+SELECT MIN(reading_datetime), MAX(reading_datetime) FROM stg_energy_raw;
+
+-- وجود مقادیر منفی یا NULL
+SELECT COUNT(*) FROM stg_energy_raw WHERE kWh < 0 OR kWh IS NULL;
+
+-- نمونه داده
+SELECT TOP 10 * FROM stg_energy_raw;
